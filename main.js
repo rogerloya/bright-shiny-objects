9b9dbe2bf59dccd5cd5c19c45722b4ba8f3ef56e
rogerloya <31164770+rogerloya@users.noreply.github.com>
2021-10-09 12:09:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,75 @@
 // │ Your code here! │
 // └─────────────────┘
 
+function createUser(first, last){
+    const user = {
+        firstName: first,
+        lastName: last
+    };
+    return user;
+}
 
+function setAge(user, n){
 
+    user['age'] = n
+    return user;
+}
 
+function incrementAge(user){
 
+    user['age'] = user['age'] + 1
+    return user;
+}
 
+function fixCar(car){
 
+    car['needsMaitenance'] = false
+    return car;
+}
 
+function addGrades(student, newGrades){
+    for (let i = 0; i < newGrades.length; i++) {
+        student['grades'].push(newGrades[i])
+    }
+    
+    return student;
+}
+
+function getDataType(car, input){
+    return typeof car[input]
+}
+
+function addTodo(todos, newTodo){
+    todos.push(newTodo)
+    return todos;
+}
+
+function addSong(playlist, newSong){
+    playlist['songs'].push(newSong)
+
+    playlist['duration'] = playlist['duration'] + newSong['duration']
+    return playlist
+}
+
+function updateReportCard(reportCard, newGrade) {
+    reportCard['grades'].push(newGrade)
+    reportCard['lowestGrade'] = Math.min.apply(Math, reportCard['grades'])
+
+    reportCard['highestGrade'] = Math.max.apply(Math, reportCard['grades'])
+
+    let sum = 0
+    let d = reportCard['grades'].length
+    let average = sum/d
+
+    for (let i = 0; i < reportCard['grades'].length; i++) {
+        sum += reportCard['grades'][i]
+    }
+
+    reportCard['averageGrade'] = sum/d
+
+    return reportCard
+
+}
 
 
 // ┌─────────────────────────────────────────────────────────────────────────┐
